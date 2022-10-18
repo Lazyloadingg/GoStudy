7bc55edffe27795ab0cbba8369a82b9c255b745b
lazyloading <lazyloading@163.com>
2022-10-18 23:06:49 +0800

    jsonrpche客户端,服务端使用,同步异步调用

--- rpc/client/client.go
+++ rpc/client/client.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
+type Param struct {
+	Width, Height int
+}
+
 func main() {
+	// gorpcClient()
+	jsonrpcClient()
+}
+
+// rpc客户端
+func gorpcClient() {
 	//连接远程rpc
 	client, err := rpc.DialHTTP("tcp", ":9900")
 	if err != nil {
@@ -20,5 +31,29 @@ func main() {
 		return
 	}
 	fmt.Printf("TestService.Area rpc调用成功res: %v\n", res)
+}
+
+// jsonrpc客户端
+func jsonrpcClient() {
+	client, err := jsonrpc.Dial("tcp", ":9900")
+	if err != nil {
+		fmt.Printf("\"连接rpc服务失败\": %v\n", "连接rpc服务失败")
+	}
+	//定义出参
+	var res int
+	//同步调用
+	//调用远程服务 args: (服务名.方法名) ，入参，出参，返回值
+	err = client.Call("TestService.Perimeter", Param{Width: 2, Height: 4}, &res)
+	if err != nil {
+		fmt.Printf("\"TestService.Perimeter rpc调用失败\": %v--%v\n", "TestService.Perimeter rpc调用失败", err)
+		return
+	}
+	fmt.Printf("TestService.Perimeter rpc调用成功res: %v\n", res)
+
+	//异步调用
+	cn := make(chan *rpc.Call, 1)
+	client.Go("TestService.Perimeter", Param{Width: 2, Height: 4}, &res, cn)
+	x := <-cn
+	fmt.Printf("x: %v--%v\n", x, res)
 
 }
--- rpc/server/server.go
+++ rpc/server/server.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
 // 定义服务
 type TestService struct{}
 
+type Param struct {
+	Width  int
+	Height int
+}
+
 func main() {
+
+	//1. 创建服务
+	//2. 注册服务
+
+	// gorpc()
+	jsonrpcServer()
+}
+
+// gorpc，只能go语言服务之间调用，不能跨语言
+func gorpc() {
 	test := new(TestService)
 	//注册服务
 	rpc.Register(test)
@@ -22,7 +40,6 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("\"rpc监听成功\": %v\n", "rpc监听成功")
-
 }
 
 // rpc函数（必须有接收者，指定属于哪个服务）
@@ -35,4 +52,36 @@ func (s *TestService) Area(param map[string]int, res *int) error {
 	fmt.Printf("\"方法调用\": %v\n", "方法调用")
 	*res = param["width"] * param["height"]
 	return nil
+}
+
+func (s *TestService) Perimeter(args Param, reply *int) error {
+	*reply = (args.Height + args.Width) * 2
+	return nil
+}
+
+// 可以跨语言
+func jsonrpcServer() {
+	err := rpc.Register(new(TestService))
+	if err != nil {
+		fmt.Printf("\"jsonrpc注册失败\": %v\n", "jsonrpc注册失败")
+		return
+	}
+	lis, err := net.Listen("tcp", ":9900")
+
+	if err != nil {
+		fmt.Printf("\"jsonrpc监听失败\": %v\n", "jsonrpc监听失败")
+		return
+	}
+
+	for {
+		//接收连接
+		con, err := lis.Accept()
+		if err != nil {
+			continue
+		}
+		go func(conn net.Conn) {
+			log.Printf("客户端进入")
+			jsonrpc.ServeConn(conn)
+		}(con)
+	}
 }
